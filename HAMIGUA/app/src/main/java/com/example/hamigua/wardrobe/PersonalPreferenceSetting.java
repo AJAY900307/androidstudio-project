bb220b8f4b39763a48f66a681f63cf030d2c0920
AJAY900307 <berry41321@gmail.com>
2024-01-06 17:57:59 +0800

    first commit

---
+++ HAMIGUA/app/src/main/java/com/example/hamigua/wardrobe/PersonalPreferenceSetting.java
@@ -0,0 +1,16 @@
+package com.example.hamigua.wardrobe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hamigua.R;
+
+public class PersonalPreferenceSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_preference_setting);
+    }
+}
